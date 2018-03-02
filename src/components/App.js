853fbb13cabfda94a477187040f200a721759470
DimasiX <31313943+DimasiX@users.noreply.github.com>
2018-03-02 23:04:38 +0300

    Start Again

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+//import logo from '../logo.svg';
 import '../css/App.css';
 import {
   BrowserRouter,
@@ -9,46 +9,20 @@ import {
 //My Components
 import Header from './Header';
 import Home from './Home';
-
 //Variables
-let GAMES = ["Запомни на время",
-            "Угадай цвет",
-            "Считай на время",
-            "Развивай концентрацию",
-];
 
 
 
 
 class App extends Component {
-  state = {
-    input: "",
-    games: GAMES,
-  }
-  getInput = (e) => {
-    this.setState({
-      input: e.target.value
-    })
-  }
-  addGame = (e) => {
-    e.preventDefault();
-    GAMES.push(this.state.input);
-    this.setState({
-      input: ""
-    });
-  }
+
   render() {
     return (
       <BrowserRouter>
-          <div className="App">
-              <div className='wallpaper'>
-              </div>
+        <div className="app">
             <Header />
-            <Route exact path="/" render={()=> <Home inputField={this.state.input}
-                                                     games={GAMES}
-                                                     input={this.getInput}
-                                                     addGame={this.addGame}/>}/>
-          </div>
+            <Route path="/" component={Home}/>
+        </div>
       </BrowserRouter>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,36 +3,8 @@ import React, {PropTypes} from 'react';
 const Home = (props) => {
   return(
     <div className="home">
-      <div className="blur-img">
-        <div className="overflow"></div>
-      </div>
-      <h1 className="greetings">Welcome Home!</h1>
-      <div className="home-content">
-        <form onSubmit={props.addGame}>
-          <input value={props.inputField} type="text" placeholder="Type Your Name!" onChange={props.input}/>
-          <button type="submit" onClick={props.addGame}>Add On It!</button>
-        </form>
-
-        <ul>
-          {
-            props.games.map((game, index)=>{
-              return(
-                <li key={index}>{game}</li>
-              )
-            })
-          }
-        </ul>
-
-
-      </div>
+      bla bla bla
     </div>
   )
 }
-
-/*
-Home.propTypes = {
-  input: PropTypes.func.isRequired,
-  addGame: PropTypes.func.isRequired
-}
-*/
 export default Home;
