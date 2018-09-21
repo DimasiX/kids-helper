e62bde510056176d3775b860137198da627e85b2
Dima Baranov <vajnoe-dima@mail.ru>
2018-09-21 14:16:45 +0300

    add types completion

--- src/app/components/HomePage/Wish/Menu/MenuDots.jsx
+++ src/app/components/HomePage/Wish/Menu/MenuDots.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { OpenMenu } from "../../../../../store/reducers/reducer.wishes";
-
 class MenuDots extends Component {
   render() {
     const { wishKey, OpenMenu } = this.props;
